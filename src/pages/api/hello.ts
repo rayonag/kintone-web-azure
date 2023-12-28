ed48ab7141316b48af943bacb3792169cd62b2e1
Ray <ronaga@bridgesforpeace.com>
2023-12-28 10:50:38 +0200

    commit

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import sgMail from "@sendgrid/mail";
+
 type Data = {
     name: string;
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    res.status(200).json({ name: "John Doe" });
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+    const msg = {
+        to: "onaga.ray@gmail.com", // Change to your recipient
+        from: "<Ray Onaga>ronaga@bridgesforpeace.com", // Change to your verified sender
+        subject: "Sending with SendGrid is Fun",
+        text: "and easy to do anywhere, even with Node.js",
+        html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    };
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log("Email sent");
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    res.status(200).json({ name: process.env.SENDGRID_API_KEY! });
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,20 +7,25 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     const [message, setMessage] = useState("");
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch("/api/hello");
-            const data = await response.json();
-            console.log(data);
-            setMessage(data.name);
-        };
 
-        fetchData();
-    }, []);
+    const sendEmail = async () => {
+        const response = await fetch("/api/hello");
+        const data = await response.json();
+        console.log(data);
+    };
+    const fetchKintone = async () => {
+        const response = await fetch("/api/kintone");
+        const data = await response.json();
+        console.log(data);
+    };
+
     return (
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <button onClick={() => console.log(message)}>Click me!</button>
+                <button onClick={() => fetchKintone()}>Fetch kintone</button>
+                <div>
+                    <button onClick={async () => sendEmail()}>Send Email</button>
+                </div>
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     Get started by editing&nbsp;
                     <code className="font-mono font-bold">src/pages/index.tsx</code>
