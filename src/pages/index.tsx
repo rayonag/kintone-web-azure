9ec9e08ed8dc4ff58777bcb3fddff5ccd91bfe84
Ray <ronaga@bridgesforpeace.com>
2023-12-28 12:15:19 +0200

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     return (
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <button onClick={() => fetchKintone()}>Fetch kintone</button>
+                <button onClick={() => fetchKintone()}>Festch kintone</button>
                 <div>
                     <button onClick={async () => sendEmail()}>Send Email</button>
                 </div>
