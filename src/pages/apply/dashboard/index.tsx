b592a164373e24c90ba8706384a5b0ffb37e765a
Ray Onaga <onaga.ray@gmail.com>
2024-01-05 13:04:01 +0200

    commit

--- src/pages/apply/dashboard/index.tsx
+++ src/pages/apply/dashboard/index.tsx
@@ -36,15 +36,17 @@ const Dashboard = () => {
                         <h1 className="text-xl my-10">Welcome back, {loginUser?.name}!</h1>
                     </div>
                     {/* Moving sentence */}
-                    <div className="absolute top-0 left-0 right-0 text-left mt-4 moveSentence">
+                    <div className="absolute top-5 left-0 right-0 text-left items-baseline mt-4 moveSentence">
                         <div className="flex text-2xl ">
-                            <img src="https://www.bridgesforpeace.com/wp-content/themes/bridges4peace/images/logo.jpg" style={{ maxWidth: "10rem" }} alt="Bridges for Peace"></img>
+                            <img src="https://www.bridgesforpeace.com/wp-content/themes/bridges4peace/images/logo.jpg" style={{ maxWidth: "7rem" }} alt="Bridges for Peace"></img>
+                            <p className="text-2xl italic text-white ml-5">Bridges for Peace</p>
+                            <p className="text-2xl italic text-white ml-5">גשרים למעו השלום</p>
+                            <p className="text-2xl italic text-blue-300 ml-5">... Your Israel Connection</p>
                         </div>
-                        <div className="text-xl text-blue-300">... Your Israel Connection</div>
                     </div>
                     {/* Content */}
                     <div className="flex flex-col items-center justify-center">
-                        <div>This page will show how far your application is...</div>
+                        <div>This page will show the progress of your application ...</div>
                         <ul className="list-none p-0">
                             <li className="flex items-center mb-2">
                                 <span className="text-green-500 text-2xl mr-2">&#10004;</span>
@@ -66,9 +68,13 @@ const Dashboard = () => {
                         <button className="btn">Contact Us</button>
                     </div>
                     {/* Moving sentence */}
-                    <div className="absolute bottom-5 left-0 right-0 text-left mt-4 moveSentenceRev">
-                        <p className="text-2xl italic text-white">Bridges for Peace</p>
-                        <p className="text-2xl italic text-white">גשרים למעו השלום</p>
+                    <div className="absolute bottom-5 left-0 right-0 text-left items-baseline mt-4 moveSentenceRev">
+                        <div className="flex text-2xl ">
+                            <img src="https://www.bridgesforpeace.com/wp-content/themes/bridges4peace/images/logo.jpg" style={{ maxWidth: "7rem" }} alt="Bridges for Peace"></img>
+                            <p className="text-2xl italic text-white ml-5">Bridges for Peace</p>
+                            <p className="text-2xl italic text-white ml-5">גשרים למעו השלום</p>
+                            <p className="text-2xl italic text-blue-300 ml-5">... Your Israel Connection</p>
+                        </div>
                     </div>
                 </>
             </div>
