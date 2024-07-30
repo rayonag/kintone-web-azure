8ef3b1ba4c9ca6784eec67b2125762b86d653f2a
ronaga <onaga.ray@gmail.com>
2024-07-30 12:06:43 +0300

    Rate Us fix

--- src/components/modal/RateUs.tsx
+++ src/components/modal/RateUs.tsx
@@ -37,7 +37,7 @@ const RateUs = () => {
 
     const handleSubmit = () => {
         const name = dashboardUser.name;
-        fetch('/api/sendgrid/contactUs', {
+        fetch('/api/sendgrid/rateUs', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
