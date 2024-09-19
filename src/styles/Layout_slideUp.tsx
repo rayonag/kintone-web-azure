01dad31ad94dfbb0276c96ad55db3a1c74a920b1
Ray Onaga <onaga.ray@gmail.com>
2024-09-19 14:16:46 +0300

    isZealous fix

--- src/styles/Layout_slideUp.tsx
+++ src/styles/Layout_slideUp.tsx
@@ -3,6 +3,7 @@
 import { FC, ReactNode } from 'react';
 import { motion } from 'framer-motion';
 import Header from '@/components/header menu/Header';
+import useUserStore from '@/features/common/store';
 
 interface Layout_slideUpProps {
     key?: string;
@@ -10,9 +11,11 @@ interface Layout_slideUpProps {
 }
 
 const Layout_slideUp: FC<Layout_slideUpProps> = ({ key, children }) => {
+    const type = useUserStore((state) => state.applicationType);
+    const isZealous = type === 'Zealous';
     return (
         <motion.div key={key || 'page'} initial={{ opacity: 0, y: '10%' }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1.2 }}>
-            <Header />
+            <Header isZealous={isZealous} />
             {children}
         </motion.div>
     );
