d4d78e75809f418b3eb8a4f15559b40a5d953123
Ray Onaga <onaga.ray@gmail.com>
2024-11-05 10:10:35 +0200

    typo

--- src/features/common/forms/reference/views/Step1.tsx
+++ src/features/common/forms/reference/views/Step1.tsx
@@ -37,7 +37,7 @@ const Step1: FC<Step1Props> = ({ register, errors, getValues, t, control }) => {
                 </div>
                 <div className="text-2xl my-4 pl-4 text-gray-600">Before you start...</div>
                 <div className="text-sm md:text-base p-4 text-gray-600">
-                    You will need to complete the Volunteer Refernce form in one go - you cannot save your progress and continue later. If you close
+                    You will need to complete the Volunteer Reference form in one go - you cannot save your progress and continue later. If you close
                     the browser tab your answers will be deleted and you will need to start again.
                 </div>
                 <div className="text-xl text-center mt-24 mb-4 text-gray-600">Scroll down to continue</div>
